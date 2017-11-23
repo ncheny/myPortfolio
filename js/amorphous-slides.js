4a72c723f310d8e62b8900efaccd1b7cea4eb796
Unknown <nancy_chen107@hotmail.com>
2017-11-23 04:32:34 -0800

    Added slick library to help with carousel making

---
+++ js/amorphous-slides.js
@@ -0,0 +1,21 @@
+  $(document).ready(function() {
+    $('.slides').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      cssEase: 'linear'
+    });
+
+    $('.concept-slides').slick({
+      infinite: true,
+slidesToShow: 2,
+slidesToScroll: 2,
+arrows: true,
+dots: true,
+
+  });
+  });
